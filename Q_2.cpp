68b5d9ea0ba3f38962cf7399843ad213eef88da0
demir <69163510+demirbaydemir@users.noreply.github.com>
2023-11-13 15:20:57 +0300

    Add files via upload

---
+++ Q_2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void perf_num(int num);
+
+int main(void)
+{
+    // Define variables
+    int num;
+
+    // Get user input
+    cout << "Input a integer value:\n";
+    cin >> num;
+
+    // Check user input
+    while (num <= 0)
+    {
+        cout << "Invalid input, try again:\n";
+        cin >> num;
+    }
+
+    // Print results
+    perf_num(num);
+
+}
+
+void perf_num(int num)
+{
+    int counter = 0;
+    
+    for (int i = 1; i < num; i++)
+    {
+        for (int j = 1; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                counter = counter + j;
+            }
+        }
+
+        if (counter == i)
+        {
+            cout << i << " ";
+        }
+
+        counter = 0;
+    }
+}
